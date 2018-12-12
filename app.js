46893def59fec2e56fbe9792ae519eb7375fc135
unknown <AasisKarkee00@gmail.com>
2018-12-12 11:53:58 +0545

    This is array in javascript

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-$(document).ready(function(){
-  $('input').focus(function(){
-  $(this).css('background','pink');
-  })
-$('input').blur(function(){
-  $(this).css('background','white');
- })
-  
- })
-
 var inputArray = [];
-var titleinput = document.getElementById('title');
-var nameinput = document.getElementById('name');
-var ticketsinput = document.getElementById('tickets');
-var displayinput = document.getElementById('display');
+var username = document.getElementById('username');
+var email = document.getElementById('email');
+var brday = document.getElementById('brday');
+var job = document.getElementById('job');
+var screen = document.getElementById('screen');
 
-document.getElementById('submit').onclick = function(){
-  inputArray.push(titleinput.value);
-  inputArray.push(nameinput.value);
-  inputArray.push(ticketsinput.value);
-  displayinput.innerHTML = titleinput.value + nameinput.value + ticketsinput.value;
+document.getElementById('button').onclick = function(){
+  inputArray.push(username.value);
+  inputArray.push(email.value);
+  inputArray.push(brday.value);
+  inputArray.push(job.value);
+  screen.innerHTML = username.value + email.value + brday.value + job.value;
 
-  document.cookie = "USER=" + inputArray;
-
+  document.cookie = "USER=" + inputArray + "expires="+ new Date(new Date().getTime()+60*60*1000*24).toGMTString()+";path=/";
 };
+
+
+
+
+
+
+
+
+
+
 
 
 
